5990328625338ef101e651c0bca1c4dfe2d7fd9a
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-16 15:40:47 +0500

    Bamk account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double money = 1000;
+        int coint = 1;
+        double profit;
+        while (coint <= 12) {
+            money += money/100*2;
+            profit = money - 1000;
+            coint++;
+            System.out.println("Прирост суммы каждый месяц " + (int)profit);
+            if (coint >= 3){
+                System.out.println("Сумма вклада "+(int)money);
+            }
+        }
+
     }
 }
